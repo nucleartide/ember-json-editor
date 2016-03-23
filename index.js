85cd1528a1ec36b4d5083ec8651948f65feb2dae
Jason Tu <jasontu4@gmail.com>
2016-03-23 18:33:38 -0400

    add bower imports

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+
 /* jshint node: true */
 'use strict';
 
 module.exports = {
-  name: 'ember-json-editor'
-};
+  name: 'ember-json-editor',
+
+  included: function(app) {
+    this._super.included(app)
+    app.import(app.bowerDirectory + '/jsoneditor/dist/jsoneditor.js')
+    app.import(app.bowerDirectory + '/jsoneditor/dist/jsoneditor.css')
+    app.import(app.bowerDirectory + '/jsoneditor/dist/img/jsoneditor-icons.svg')
+  }
+}
+

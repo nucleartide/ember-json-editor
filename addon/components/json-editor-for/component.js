0423c5c1c9a8e62c4f624250990b62d2e66dbb8a
Jason Tu <jasontu4@gmail.com>
2016-03-24 17:20:30 -0400

    mirror all jsoneditor properties

--- addon/components/json-editor-for/component.js
+++ addon/components/json-editor-for/component.js
@@ -3,6 +3,8 @@ import Ember from 'ember'
 import layout from './template'
 import JSONEditor from 'jsoneditor'
 
+const { K } = Ember
+
 const JSONEditorFor = Ember.Component.extend({
 
   /**
@@ -15,14 +17,51 @@ const JSONEditorFor = Ember.Component.extend({
   // @type {JSON}
   json: null,
 
-  // @type {Function}
-  update: null,
-
   // @type {Object}
   ace: null,
 
   // @type {Object}
   ajv: null,
+
+  // @type {Function}
+  onChange: null,
+
+  // @type {Function}
+  onEditable: null,
+
+  // @type {Function}
+  onError: K,
+
+  // @type {Function}
+  onModeChange: null,
+
+  // @type {Boolean}
+  escapeUnicode: false,
+
+  // @type {Boolean}
+  history: true,
+
+  // @type {String}
+  mode: 'tree',
+
+  // @static
+  // @type {Array<String>}
+  modes: ['tree', 'view', 'form', 'code', 'text'],
+
+  // @type {String}
+  name: '',
+
+  // @type {Object}
+  schema: null,
+
+  // @type {Boolean}
+  search: true,
+
+  // @type {Number}
+  indentation: 2,
+
+  // @type {String}
+  theme: 'ace/theme/jsoneditor',
 
   /**
    * Properties.
@@ -38,9 +77,14 @@ const JSONEditorFor = Ember.Component.extend({
    * Callbacks.
    */
 
-  onChange() {
-    this.get('update')(this.editor.get())
-    console.log('change:', this.get('json'))
+  onJSONChange() {
+    try {
+      const json = this.editor.get()
+      this.get('onChange')(json)
+      console.log('change:', this.get('json'))
+    } catch (err) {
+      this.get('onError')(err)
+    }
   },
 
   didInsertElement() {
@@ -50,7 +94,9 @@ const JSONEditorFor = Ember.Component.extend({
     const json      = this.get('json')
 
     this.editor = new JSONEditor(container, {
-      onChange: this.onChange.bind(this)
+      onChange: this.onJSONChange.bind(this),
+      onError:  this.get('onError'),
+      modes:    this.get('modes')
     }, json)
   },
 
--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -7,6 +7,12 @@ export default Ember.Route.extend({
       "hello": "jason",
       "hi":    "jesse"
     }
+  },
+
+  actions: {
+    onError(err) {
+      console.error(err.stack)
+    }
   }
 })
 
